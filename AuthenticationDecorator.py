93ea8488c03f9b0c2c57d77b96ca673c8628f3fb
ElonTusk404 <igmaflappy@gmail.com>
2023-07-14 14:27:47 +0400

    Update AuthenticationDecorator.py

---
+++ AuthenticationDecorator.py
@@ -0,0 +1,7 @@
+def authentication_decorator(foo):
+    def wrapper(*args):
+        if user.auth:
+            return foo(*args)
+        else:    
+            raise Exception('User is not authenticated.')
+    return wrapper
